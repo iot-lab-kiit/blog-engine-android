cc82e09b10cdf99b2ead1120d0afaa6a473cee85
nks102000 <nitishks1110@gmail.com>
2021-10-06 11:29:04 +0530

    toolbar made

---
+++ app/src/main/java/in/iot/lab/blogengine/ui/Bloglist.kt
@@ -0,0 +1,20 @@
+package `in`.iot.lab.blogengine.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import in .iot.lab.blogengine.R
+
+
+class Bloglist : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view= inflater.inflate(R.layout.fragment_bloglist, container, false)
+        return view
+    }
+}
